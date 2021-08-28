3dfd4f1cc61aaf5b7f7498aac6fb10302e5d17bb
natata7 <natata7@gmail.com>
2021-08-28 19:30:44 +0300

    feat: add product-api on Lambda

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
-	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	product: 'https://iqs0n6evpg.execute-api.eu-west-1.amazonaws.com/dev',
+	order: 'https://iqs0n6evpg.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://iqs0n6evpg.execute-api.eu-west-1.amazonaws.com/dev',
+	bff: 'https://iqs0n6evpg.execute-api.eu-west-1.amazonaws.com/dev',
+	cart: 'https://iqs0n6evpg.execute-api.eu-west-1.amazonaws.com/dev',
 };
